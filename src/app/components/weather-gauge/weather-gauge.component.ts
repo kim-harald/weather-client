896f1d44c5c57079781cb682e47266cd493cbbe6
kim-harald <kim@sticos.no>
2022-11-03 21:34:43 +0100

    gauge with html canvas

--- src/app/components/weather-gauge/weather-gauge.component.ts
+++ src/app/components/weather-gauge/weather-gauge.component.ts
@@ -1,5 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ChartType } from 'angular-google-charts';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-weather-gauge',
@@ -7,27 +6,47 @@ import { ChartType } from 'angular-google-charts';
   styleUrls: ['./weather-gauge.component.scss'],
 })
 export class WeatherGaugeComponent implements OnInit {
-  constructor() {}
+  constructor() { }
+
+  @ViewChild('gauge', { static: true })
+  canvas: ElementRef<HTMLCanvasElement> | undefined;
 
   @Input()
-  public min:number = 0;
-
-  @Input() 
-  public max:number = 100;
+  public min: number = 0;
 
   @Input()
-  public value:number = 0;
+  public max: number = 100;
 
   @Input()
-  public units:string = 'C';
+  public value: number = 0;
 
-  public get angle():string {
-    return '-45deg';
+  @Input()
+  public units: string = 'C';
+
+  private ctx: CanvasRenderingContext2D | undefined | null;
+
+  ngOnInit(): void {
+    this.ctx = this.canvas?.nativeElement.getContext('2d');
+    // annulus(this.ctx!, 0, 0, 100, 200, 0, 180, false);
+    annulus(this.ctx!, 100, 100, 100, 180, 0, false);
+    this.ctx!.strokeStyle = "#FF0000"
+    this.ctx?.stroke();
   }
-
-  public get angle1():string {
-    return '21.6deg';
-  }
-
-  ngOnInit(): void {}
 }
+
+
+const annulus = (ctx: CanvasRenderingContext2D, centerX: number, centerY: number,
+  width: number,
+  startAngle: number, endAngle: number,
+  anticlockwise: boolean) => {
+
+  ctx.lineWidth = width * 0.2;
+  let r = width * 0.65;
+
+  var th1 = startAngle * Math.PI / 180;
+  var th2 = endAngle * Math.PI / 180;
+
+
+  ctx.arc(centerX, centerY, r, th1, th2, false);
+
+};
