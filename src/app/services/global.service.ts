c9510e5b68680a5e7d5e8f3374683feb8c1bfe9d
Kim Dadswell <kim@sticos.no>
2023-09-04 11:30:41 +0200

    add new method to globals

--- src/app/services/global.service.ts
+++ src/app/services/global.service.ts
@@ -1,33 +1,26 @@
 import { Injectable } from '@angular/core';
 import { MqttService } from 'ngx-mqtt';
-import { concatMap, forkJoin, map, merge, mergeMap, Observable, of } from 'rxjs';
-import { LocationReading } from '../models/locationreading';
-import { Reading } from '../models/reading';
-import { ApiService } from './api.service';
+import { concatMap, map, Observable } from 'rxjs';
+import { SummaryReading, SummaryService } from '../openapi';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GlobalService {
 
-  constructor(private readonly apiService:ApiService, private readonly mqttService:MqttService) { }
+  constructor(private readonly summaryService:SummaryService, private readonly mqttService:MqttService) { }
 
-  public getCurrent(location: string,startDate:Date): Observable<LocationReading[]> {
-    const mqtt$ = this.mqttService.observe('gimel/sensor/all').pipe(
-      map(mqttMessage => {
-        const data = JSON.parse(mqttMessage.payload.toString()) as Reading;
-        const locationReading = {
-          ...data,
-          location:location
-        } as LocationReading;
-        return [locationReading]
-      })
+  public getHourlySummary(location:string):Observable<SummaryReading[]> {
+    const path = `${location}/summary`;
+    return this.mqttService.observe(path).pipe(
+      concatMap(()=> {
+        const now = new Date();
+        const end = now.valueOf();
+        now.setHours(now.getHours()-48);
+        const start= now.valueOf();
+        return this.summaryService.getHourlySummary(location, start,end);
+      }),
+      map(summaryReading => summaryReading.data)
     );
-
-    const http$ = this.apiService.getReadings(location,startDate,new Date())
-
-    const result$ = merge(http$,mqtt$);
-
-    return result$;
   }
 }
